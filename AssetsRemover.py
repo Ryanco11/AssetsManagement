7ac761fb7ef859676960052dbdba1e2ffcaa0864
wcb <ytlzwcb@gmail.com>
2021-02-24 14:59:15 +0800

    save

---
+++ AssetsRemover.py
@@ -0,0 +1,12 @@
+import openpyxl
+import os
+from pathlib import Path
+from openpyxl.styles import Color, PatternFill, Font, Border
+
+
+excel_path = r'/Users/ryanco/Desktop/资源元表/服饰元表Excel.xlsx'
+project_path = r'/Users/ryanco/Projects/AndoidProject/wonder_party/avatarProject/'
+prefab_path = r'/Users/ryanco/Projects/AndoidProject/wonder_party/avatarProject/Assets/Art/BundleResources/Dress'
+assets_path = r'/Users/ryanco/Projects/AndoidProject/wonder_party/avatarProject/Assets/Art/model/coat'
+sprite_path = r'/Users/ryanco/Projects/AndoidProject/wonder_party/avatarProject/Assets/Art/BundleResources/Sprites'
+
